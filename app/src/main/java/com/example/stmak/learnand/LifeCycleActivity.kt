bd874eede0c54f556c7aabea9aee8fd6b9b69ff0
Andrey Berezhnoi <stmaktavish@gmail.com>
2018-09-04 14:41:50 +0300

    ended logic of recycler view

--- app/src/main/java/com/example/stmak/learnand/LifeCycleActivity.kt
+++ app/src/main/java/com/example/stmak/learnand/LifeCycleActivity.kt
@@ -2,7 +2,8 @@ package com.example.stmak.learnand
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_life_cycle.*
 
 class LifeCycleActivity : AppCompatActivity() {
@@ -12,17 +13,25 @@ class LifeCycleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
 
-
+        updItemList("onCreate()")
     }
 
     override fun onStart() {
         super.onStart()
 
+        updItemList("onStart()")
     }
 
     override fun onResume() {
         super.onResume()
 
+        updItemList("onResume()")
+    }
+
+    private fun updItemList(txt: String){
+        itemList.add(txt)
+        recycler_view_lifecycle.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        recycler_view_lifecycle.adapter = LifeCycleAdapter(itemList)
     }
 
 }
