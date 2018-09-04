d78aba554d5c22652bc4f6bb15e0e9398d6561b8
Andrey Berezhnoi <stmaktavish@gmail.com>
2018-09-04 13:49:28 +0300

    added first onCreate text when app is create

--- app/src/main/java/com/example/stmak/learnand/LifeCycleActivity.kt
+++ app/src/main/java/com/example/stmak/learnand/LifeCycleActivity.kt
@@ -2,11 +2,15 @@ package com.example.stmak.learnand
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_life_cycle.*
 
 class LifeCycleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
+
+        tv_lifecycle.text = "onCreate()"
     }
+
 }
