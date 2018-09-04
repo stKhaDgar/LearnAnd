1a6db5a259a2baaed2b73a630a6e6bb384bb556f
Andrey Berezhnoi <stmaktavish@gmail.com>
2018-09-04 13:14:18 +0300

    added new act lifecycle and logic to first bt main

--- app/src/main/java/com/example/stmak/learnand/MainActivity.kt
+++ app/src/main/java/com/example/stmak/learnand/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.stmak.learnand
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onClickListener()
+    }
+
+    private fun onClickListener(){
+        bt_lifecycle.setOnClickListener {
+            startActivity(Intent(this, LifeCycleActivity::class.java))
+        }
     }
 }
